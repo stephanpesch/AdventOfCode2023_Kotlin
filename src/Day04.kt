98679cf164acf13216d35b84bb30733b38133f5d
stephanpesch <stephan.pesch1@gmail.com>
2023-12-04 22:36:44 +0100

    Solved Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -9,12 +9,14 @@ fun main() {
         }
         .collect(Collectors.toSet())
 
-    fun parseInput(input: List<String>): Int {
-        return input.stream()
+    fun splitSides(input: String) = input
+        .substringAfter(":")
+        .split("|")
+
+    fun parseInput1(input: List<String>): Int {
+        return input.parallelStream()
             .map {
-                val sides = it
-                    .substringAfter(":")
-                    .split("|")
+                val sides = splitSides(it)
                 val leftSide = sides[0].toIntSet()
                 val rightSide = sides[1].toIntSet()
                 val inter = leftSide intersect rightSide
@@ -27,17 +29,36 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        return parseInput(input)
+        return parseInput1(input)
     }
 
+    fun parseInput2(input: List<String>): Int {
+        val winList = input.stream()
+            .map {
+                val sides = splitSides(it)
+                val leftSide = sides[0].toIntSet()
+                val rightSide = sides[1].toIntSet()
+                val inter = leftSide intersect rightSide
+                inter.size
+            }
+            .collect(Collectors.toList())
+        val cardAmountList = List(winList.size) { 1 }.toMutableList()
+        winList.forEachIndexed { index, wins ->
+            for (i in index until index + wins) {
+                cardAmountList[i + 1] = cardAmountList[i + 1] + cardAmountList[index]
+            }
+        }
+        return cardAmountList.sum()
+    }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseInput2(input)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
